a727d7a8cccc8b09f158dbb8bf19a6946f91a358
Omkar (Ahaan) Waghmare <91741647+omkar7075@users.noreply.github.com>
2025-03-17 16:01:13 +0530

    app.py

--- app.py
+++ app.py
@@ -126,8 +126,6 @@ def predict():
         })
 
 if __name__ == "__main__":
-    from waitress import serve
-    port = int(os.environ.get('PORT', 5000))
-    serve(app, host="0.0.0.0", port=port)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
